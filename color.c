d978fe3dddbf5ad30a11c82b0f80f578f725c238
Helene BRULIN <hbrulin@e3r9p6.42.fr>
2020-01-13 18:11:25 +0100

    check rgb

--- color.c
+++ color.c
@@ -6,7 +6,7 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 14:05:41 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/01/11 18:11:54 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/01/13 18:06:44 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,25 +27,30 @@ int	get_color(t_env *env, t_color *color_floor, t_color *color_ceiling)
 	color_floor->rgb.g = ft_atoi(tab[1]);
 	color_floor->rgb.b = ft_atoi(tab[2]);
 	color_floor->rgb.a = 0x00;
-	ft_tabdel((void **)tab); //attention possible leak ici avec mon split, verifier mon tabdel
+	ft_tabdel((void **)tab); //attention possible leak ici avec mon split
 
-	//verifier aussi que rgb < 255
+	//c'est quoi cette condition
 	if ((!color_floor->rgb.r && color_floor->rgb.r != 0) || (!color_floor->rgb.g && color_floor->rgb.g != 0) || (!color_floor->rgb.b && color_floor->rgb.b != 0)) //autre chose a verifier quand a mauvais input (genre si pas de virgule entre les valeurs dans le doc map)
+		return (INVALID_ARG);
+	if (color_floor->rgb.r > 255 || color_floor->rgb.r < 0 || color_floor->rgb.g > 255 || color_floor->rgb.g < 0 || color_floor->rgb.b > 255 || color_floor->rgb.b < 0 )
 		return (INVALID_ARG);
 
 	i = 0;
 	while (!ft_isdigit(env->data.C[i]))
 		i++;
-	if(!(tab = ft_split(env->data.C + i, ',')))
+	if(!(tab = ft_split(env->data.C + i, ','))) // quid si pas de virgule???
 		return (MALLOC_FAIL);
 	color_ceiling->rgb.r = ft_atoi(tab[0]);
 	color_ceiling->rgb.g = ft_atoi(tab[1]);
 	color_ceiling->rgb.b = ft_atoi(tab[2]);
 	color_ceiling->rgb.a = 0x00;
-	ft_tabdel((void **)tab); //attention possible leak ici avec mon split, verifier mon tabdel
+	ft_tabdel((void **)tab); //attention possible leak ici avec mon split
 	
-	//verifier aussi que rgb valide
-	if ((!color_ceiling->rgb.r && color_ceiling->rgb.r != 0) || (!color_ceiling->rgb.g && color_ceiling->rgb.b != 0) || (!color_ceiling->rgb.b && color_ceiling->rgb.b != 0))//autre chose a verifier quand a mauvais input (genre si pas de virgule entre les valeurs dans le doc map)
+	//c'est quoi cette condition??
+	if ((!color_ceiling->rgb.r && color_ceiling->rgb.r != 0) || (!color_ceiling->rgb.g && color_ceiling->rgb.g != 0) || (!color_ceiling->rgb.b && color_ceiling->rgb.b != 0))
 		return (INVALID_ARG);
+	if (color_ceiling->rgb.r > 255 || color_ceiling->rgb.r < 0 || color_ceiling->rgb.g > 255 || color_ceiling->rgb.g < 0 || color_ceiling->rgb.b > 255 || color_ceiling->rgb.b < 0 )
+		return (INVALID_ARG);
+	
 	return (SUCCESS);
 }

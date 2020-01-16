ef6b6ff32e5f324a826f173b2f9c9331a8412883
Helene BRULIN <hbrulin@e2r7p12.42.fr>
2020-01-16 19:48:53 +0100

    more norm

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 13:48:09 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/01/16 19:42:28 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/01/16 19:48:33 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,12 @@ char	*ft_strdup_no_space(const char *s)
 {
 	int		s_len;
 	char	*dst;
-	int i;
-	int count;
+	int		i;
+	int		count;
 
 	i = 0;
 	count = 0;
-	while(s[i])
+	while (s[i])
 	{
 		if (s[i] == ' ')
 			count++;
@@ -58,12 +58,14 @@ char	*ft_strdup_no_space(const char *s)
 
 t_img	*ft_new_image(t_env *env, int width, int height)
 {
-	t_img *img; 
+	t_img *img;
+
 	if (!(img = malloc(sizeof(t_img))))
 		return (NULL);
-	if(!(img->img_ptr = mlx_new_image (env->mlx_ptr, width, height)))
+	if (!(img->img_ptr = mlx_new_image(env->mlx_ptr, width, height)))
 		return (NULL);
-	if(!(img->img_data = (int *)mlx_get_data_addr(img->img_ptr, &img->bpp, &img->size_line, &img->endian)))
+	if (!(img->img_data = (int *)mlx_get_data_addr(img->img_ptr, &img->bpp,
+		&img->size_line, &img->endian)))
 		return (NULL);
 	img->width = width;
 	img->height = height;
@@ -73,11 +75,14 @@ t_img	*ft_new_image(t_env *env, int width, int height)
 t_tex	*ft_new_tex(t_env *env, char *file)
 {
 	t_tex *tex;
+
 	if (!(tex = malloc(sizeof(t_tex))))
 		return (NULL);
-	if(!(tex->tex_ptr = mlx_xpm_file_to_image(env->mlx_ptr, file, &tex->width, &tex->height)))
+	if (!(tex->tex_ptr = mlx_xpm_file_to_image(env->mlx_ptr, file,
+		&tex->width, &tex->height)))
 		return (NULL);
-	if(!(tex->tex_data = (int *)mlx_get_data_addr(tex->tex_ptr, &tex->bpp, &tex->size_line, &tex->endian)))
+	if (!(tex->tex_data = (int *)mlx_get_data_addr(tex->tex_ptr, &tex->bpp,
+		&tex->size_line, &tex->endian)))
 		return (NULL);
 	return (tex);
 }

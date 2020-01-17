deca2c23047d239d0ddf8b106c57b21f36dc8447
Helene BRULIN <hbrulin@e3r11p19.42.fr>
2020-01-17 16:39:38 +0100

    fix parse rgb

--- color.c
+++ color.c
@@ -6,7 +6,7 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 14:05:41 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/01/16 19:44:34 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/01/17 16:39:01 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,28 +36,43 @@ void	pix_color(t_env *env)
 	}
 }
 
-int		get_color(t_color *color, char *s)
+int check_color(char **tab)
 {
-	int		i;
-	char	**tab;
-
-	i = 0;
-	while (!ft_isdigit(s[i]))
-		i++;
-	if (!(tab = ft_split(s + i, ',')))
-		return (MALLOC_FAIL);
+	int r;
+	int g;
+	int b;
 	if (tab_size(tab) == 3)
 	{
-		color->rgb.r = ft_atoi(tab[0]);
-		color->rgb.g = ft_atoi(tab[1]);
-		color->rgb.b = ft_atoi(tab[2]);
-		color->rgb.a = 0x00;
+		r = ft_atoi(tab[0]);
+		g = ft_atoi(tab[1]);
+		b = ft_atoi(tab[2]);
 	}
 	else
 	{
 		ft_tabdel((void **)tab);
 		return (WRONG_INPUT);
 	}
+	if (r > 255 || r < 0 || g > 255 || g < 0 || b > 255 || b < 0)
+		return (WRONG_INPUT);
+	return (SUCCESS);
+}
+
+int		get_color(t_color *color, char *s)
+{
+	int		i;
+	char	**tab;
+	
+	i = 0;
+	while (!ft_isdigit(s[i]) && s[i] != '-')
+		i++;
+	if (!(tab = ft_split(s + i, ',')))
+		return (MALLOC_FAIL);
+	if (check_color(tab) == WRONG_INPUT)
+		return (WRONG_INPUT);
+	color->rgb.r = ft_atoi(tab[0]);
+	color->rgb.g = ft_atoi(tab[1]);
+	color->rgb.b = ft_atoi(tab[2]);
+	color->rgb.a = 0x00;
 	ft_tabdel((void **)tab);
 	return (SUCCESS);
 }

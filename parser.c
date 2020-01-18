970eb5e33e1f49fb7bfc21daf305e8e6ce141cf5
Helene BRULIN <hbrulin@e2r10p14.42.fr>
2020-01-18 14:06:13 +0100

    norm parser

--- parser.c
+++ parser.c
@@ -6,12 +6,28 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/10 11:10:04 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/01/18 13:47:29 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/01/18 14:06:00 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include <stdio.h>
+
+void	pos_from_map(t_env *env, int i, int j)
+{
+	if (env->map.flag_n == 0 && (env->map.tab_map[i][j] == 'N' ||
+		env->map.tab_map[i][j] == 'S' || env->map.tab_map[i][j] == 'E'
+		|| env->map.tab_map[i][j] == 'W'))
+	{
+		env->map.flag_n = 1;
+		env->map.player = env->map.tab_map[i][j];
+		env->pos.x = j + 0.5;
+		env->pos.y = i + 0.5;
+		env->map.tab_map[i][j] = '0';
+	}
+	if (env->map.tab_map[i][j] == '2')
+		env->nb_sprite++;
+}
 
 int		wrong_char(t_env *env, int i, int j)
 {
@@ -24,7 +40,7 @@ int		wrong_char(t_env *env, int i, int j)
 		|| env->map.tab_map[i][j] == 'S' || env->map.tab_map[i][j] == 'E' ||
 		env->map.tab_map[i][j] == 'W'))
 		return (WRONG_MAP);
-	return(SUCCESS);
+	return (SUCCESS);
 }
 
 int		closed_map(t_env *env)
@@ -32,7 +48,6 @@ int		closed_map(t_env *env)
 	int j;
 
 	j = 0;
-
 	while (env->map.tab_map[0][j])
 	{
 		if (env->map.tab_map[0][j] != '1')
@@ -49,16 +64,12 @@ int		closed_map(t_env *env)
 	return (SUCCESS);
 }
 
-int		ft_parser(t_env *env)
+int		loop(t_env *env)
 {
 	int i;
 	int j;
 	int k;
 
-	if ((env->error = closed_map(env)) != SUCCESS)
-		return (env->error);
-	env->map.k_total = ft_strlen(env->map.tab_map[0]) +
-		ft_strlen(env->map.tab_map[env->map.nb_line - 1]);
 	i = 1;
 	j = 1;
 	while (env->map.tab_map[i] && i < env->map.nb_line)
@@ -70,26 +81,27 @@ int		ft_parser(t_env *env)
 		{
 			if ((env->error = wrong_char(env, i, j)) != SUCCESS)
 				return (env->error);
-			if (env->map.flag_n == 0 && (env->map.tab_map[i][j] == 'N' || 
-				env->map.tab_map[i][j] == 'S' || env->map.tab_map[i][j] == 'E'
-				|| env->map.tab_map[i][j] == 'W'))
-			{
-				env->map.flag_n = 1;
-				env->map.player = env->map.tab_map[i][j];
-				env->pos.x = j + 0.5; 
-				env->pos.y = i + 0.5;
-				env->map.tab_map[i][j] = '0';
-			}
-			if (env->map.tab_map[i][j] == '2')
-				env->nb_sprite++;
+			pos_from_map(env, i, j);
 			j++;
 		}
 		if (env->map.tab_map[i][0] != '1' || env->map.tab_map[i][k - 1] != '1')
 			return (WRONG_MAP);
 		i++;
 	}
-	if ((env->map.k_total % (env->map.nb_line + 1) != 0) || env->map.flag_n == 0)
-		return (WRONG_MAP);
 	env->map.nb_char = k;
+	return (SUCCESS);
+}
+
+int		ft_parser(t_env *env)
+{
+	if ((env->error = closed_map(env)) != SUCCESS)
+		return (env->error);
+	env->map.k_total = ft_strlen(env->map.tab_map[0]) +
+		ft_strlen(env->map.tab_map[env->map.nb_line - 1]);
+	if ((env->error = loop(env)) != SUCCESS)
+		return (env->error);
+	if ((env->map.k_total % (env->map.nb_line + 1) != 0) ||
+		env->map.flag_n == 0)
+		return (WRONG_MAP);
 	return (SUCCESS);
 }

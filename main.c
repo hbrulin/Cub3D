59120b5ca3ebe3023011bd36e7a9244ffad22456
Helene BRULIN <hbrulin@e2r6p7.42.fr>
2020-01-23 19:46:40 +0100

    check .cub

--- main.c
+++ main.c
@@ -6,18 +6,34 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/10 11:34:56 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/01/18 15:53:00 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/01/23 19:45:20 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-int	main(int argc, char **argv)
+int		ft_check_cub(char *s)
+{
+	int len;
+
+	len = ft_strlen(s) - 1;
+	if (s[len] != 'b')
+		return (INVALID_ARG);
+	if (s[len - 1] != 'u')
+		return (INVALID_ARG);
+	if (s[len - 2] != 'c')
+		return (INVALID_ARG);
+	if (s[len - 3] != '.')
+		return (INVALID_ARG);
+	return (SUCCESS);
+}
+
+int		main(int argc, char **argv)
 {
 	t_env	env;
 
 	ft_bzero(&env, sizeof(t_env));
-	if (argc != 2 && argc != 3)
+	if ((argc != 2 && argc != 3) || ft_check_cub(argv[1]) == INVALID_ARG)
 		return (ft_error(INVALID_ARG, &env));
 	if ((env.error = init_flag_save(&env, argc, argv[2])) != SUCCESS)
 		return (ft_error(env.error, &env));
